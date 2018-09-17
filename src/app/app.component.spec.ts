89a27658ca277aca747f45b578b70693cbb35259
alsorcam <albasornosa@gmail.com>
2018-09-17 17:33:30 +0200

    First try with component testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './pages/profile/components/profile/profile.component';
+import { AppModule } from './app.module';
+import { SharedModule } from './shared/components/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ProfileComponent
       ],
+      imports: [
+        SharedModule,
+        TranslateModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +22,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app-profile-angular'`, async(() => {
+  it(`should have as theme 'green'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app-profile-angular');
+    expect(app.selectedTheme).toEqual('green');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app-profile-angular!');
-  }));
+  // it('should render title in a h1 tag', async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to app-profile-angular!');
+  // }));
 });
