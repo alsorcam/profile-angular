76310627e39a2ffc7ffc784fd31165101c9ba9a8
alsorcam <albasornosa@gmail.com>
2018-09-17 17:32:45 +0200

    Change a thing for selected theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,9 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-profile-angular';
-  selectedTheme: string;
+  selectedTheme: string = Properties.DEFAULT_THEME;
 
   constructor(translate: TranslateService) {
-    this.selectedTheme = Properties.DEFAULT_THEME;
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
